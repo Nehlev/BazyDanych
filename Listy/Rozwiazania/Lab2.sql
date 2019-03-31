ce5b3ed6e4feefaa2aff69560739a84aad40eef5
pkorycin <pawel.korycinski@nokia.com>
2019-03-31 15:32:11 +0200

    Resolve Lab 2 up to 5

--- Listy/Rozwiazania/Lab2.sql
+++ Listy/Rozwiazania/Lab2.sql
@@ -1,10 +1,14 @@
 /* Lab2_1 */
-SELECT * FROM Telco_Wroclaw.dbo.Hardware
-SELECT * FROM Telco_Wroclaw.dbo.Operator
-SELECT * FROM Telco_Wroclaw.dbo.Software
+SELECT * FROM Telco_Wroclaw.dbo.TelcoUser
 
 /* Lab2_2 */
-SELECT Hardware.HwType, Hardware.Location from Telco_Wroclaw.dbo.Hardware
+SELECT Name, Surname FROM Telco_Wroclaw.dbo.TelcoUser
 
 /* Lab2_3 */
+SELECT CONCAT(Name, Surname) as STUDENT FROM Telco_Wroclaw.dbo.TelcoUser
 
+/* Lab2_4 */
+SELECT * FROM Telco_Wroclaw.dbo.TelcoUser ORDER BY Surname
+
+/* Lab2_5 */
+SELECT * FROM Telco_Wroclaw.dbo.TelcoUser WHERE Address_City like 'Wroclaw'
