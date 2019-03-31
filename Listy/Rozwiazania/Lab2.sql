87ccf0077330261c302426bce7851d8a004e248b
pkorycin <pawel.korycinski@nokia.com>
2019-03-31 14:06:33 +0200

    Init commit with requirements and lectures and lab pdfs

---
+++ Listy/Rozwiazania/Lab2.sql
@@ -0,0 +1,10 @@
+/* Lab2_1 */
+SELECT * FROM Telco_Wroclaw.dbo.Hardware
+SELECT * FROM Telco_Wroclaw.dbo.Operator
+SELECT * FROM Telco_Wroclaw.dbo.Software
+
+/* Lab2_2 */
+SELECT Hardware.HwType, Hardware.Location from Telco_Wroclaw.dbo.Hardware
+
+/* Lab2_3 */
+
