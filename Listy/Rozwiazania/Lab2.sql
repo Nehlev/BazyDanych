944330e90954bf620cdbabfcf91e7ff7d2a413ec
pkorycin <pawel.korycinski@nokia.com>
2019-03-31 17:59:25 +0200

    Finish Lab2

--- Listy/Rozwiazania/Lab2.sql
+++ Listy/Rozwiazania/Lab2.sql
@@ -1,17 +1,32 @@
 /* Lab2_1 */
-SELECT * FROM Telco_Wroclaw.dbo.TelcoUser
+SELECT * FROM Uczelnia.dbo.Student
 
 /* Lab2_2 */
-SELECT Name, Surname FROM Telco_Wroclaw.dbo.TelcoUser
+SELECT Imie, Nazwisko FROM Uczelnia.dbo.Student
 
 /* Lab2_3 */
-SELECT CONCAT(Name, Surname) as STUDENT FROM Telco_Wroclaw.dbo.TelcoUser
+SELECT CONCAT(Imie, Nazwisko) as STUDENT FROM Uczelnia.dbo.Student
 
 /* Lab2_4 */
-SELECT * FROM Telco_Wroclaw.dbo.TelcoUser ORDER BY Surname
+SELECT * FROM Uczelnia.dbo.Student ORDER BY Nazwisko
 
 /* Lab2_5 */
-SELECT * FROM Telco_Wroclaw.dbo.TelcoUser WHERE Address_City like 'Wroclaw'
+SELECT * FROM Uczelnia.dbo.Student WHERE Miejscowosc like 'Wroc³aw'
 
 /* Lab2_6 */
-SELECT * FROM Telco_Wroclaw.dbo.TelcoUser WHERE CONTAINS(Email, 'interia.pl')
+SELECT * FROM Uczelnia.dbo.Student WHERE CONTAINS(Email, 'interia.pl')
+
+/* Lab2_7 */
+SELECT * FROM Uczelnia.dbo.Pracownik WHERE CONTAINS(Tytul, 'Mgr')
+
+/* Lab2_8 */
+SELECT * FROM Uczelnia.dbo.Pracownik WHERE CONTAINS(Tytul, 'Dr') and Miejscowosc like 'Wroc³aw'
+
+/* Lab2_9 */
+SELECT * FROM Uczelnia.dbo.Pracownik WHERE Stanowisko like 'asystent' or Stanowisko like 'wyk³adowca'
+
+/* Lab2_10 */
+SELECT * FROM Uczelnia.dbo.Pracownik WHERE Stanowisko like 'wyk³adowca' and not CONTAINS(Tytul, 'hab')
+
+/* Lab2_11 */
+SELECT DISTINCT Miejscowosc FROM Uczelnia.dbo.Pracownik
