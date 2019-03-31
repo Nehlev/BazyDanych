d6a36e83821c896a4ac971baffcbd5f329b064be
pkorycin <pawel.korycinski@nokia.com>
2019-03-31 19:49:11 +0200

    How to solve the ambigious IdGrupy

--- Listy/Rozwiazania/Lab3.sql
+++ Listy/Rozwiazania/Lab3.sql
@@ -10,3 +10,12 @@ ON LHS.IdOddzialu = RHS.IdOddzialu
 SELECT Nazwa
 FROM Uczelnia.dbo.OddzialUczelni WHERE NOT EXISTS 
 (SELECT * FROM Uczelnia.dbo.Pracownik WHERE Uczelnia.dbo.OddzialUczelni.IdOddzialu = Uczelnia.dbo.Pracownik.IdOddzialu)
+
+SELECT * FROM Uczelnia.dbo.Student
+SELECT * FROM Uczelnia.dbo.Grupa
+
+SELECT Nazwisko, IdGrupy FROM Uczelnia.dbo.Student
+
+SELECT Nazwisko, Student.IdGrupy
+FROM Uczelnia.dbo.Student as LHS FULL OUTER JOIN Uczelnia.dbo.Grupa as RHS
+ON LHS.IdGrupy = RHS.IdGrupy
