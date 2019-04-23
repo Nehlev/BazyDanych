d8fbfcb567401fcadad8c4fcbab8313de5bedbd7
pkorycin <pawel.korycinski@nokia.com>
2019-04-23 22:39:55 +0200

    Lab3_Zad6

--- Listy/Rozwiazania/Lab3.sql
+++ Listy/Rozwiazania/Lab3.sql
@@ -1,9 +1,3 @@
-SELECT * FROM Uczelnia.dbo.Pracownik
-SELECT * FROM Uczelnia.dbo.Student
-SELECT * FROM Uczelnia.dbo.Grupa
-SELECT * FROM Uczelnia.dbo.OddzialUczelni
-SELECT * FROM Uczelnia.dbo.Przedmiot
-
 /* Lab3_2 */
 SELECT Nazwisko, Nazwa
 FROM Uczelnia.dbo.Pracownik as LHS INNER JOIN Uczelnia.dbo.OddzialUczelni as RHS 
@@ -18,9 +12,16 @@ SELECT LHS.Nazwisko, RHS.IdGrupy FROM Uczelnia.dbo.Student LHS RIGHT JOIN Uczeln
 ON LHS.IdGrupy=RHS.IdGrupy
 
 /* Lab3_5 */
-SELECT PRZEDMIOT.KodPrzedmiotu, PRZEDMIOT.Nazwa, CONCAT(PROWADZACY.RokPoczatek, '/', PROWADZACY.RokKoniec) AS RokAkademicki,/* Rok Akademicki */ PRZEDMIOT.Semestr, PRACOWNIK.Nazwisko
+SELECT PRZEDMIOT.KodPrzedmiotu, PRZEDMIOT.Nazwa, CONCAT(PROWADZACY.RokPoczatek, '/', PROWADZACY.RokKoniec) AS RokAkademicki, PRZEDMIOT.Semestr, PRACOWNIK.Nazwisko
 FROM Uczelnia.dbo.Przedmiot PRZEDMIOT
 INNER JOIN Uczelnia.dbo.ProwadzacyPrzedmiot PROWADZACY ON PROWADZACY.KodPrzedmiotu=PRZEDMIOT.KodPrzedmiotu
 INNER JOIN Uczelnia.dbo.Pracownik PRACOWNIK ON PRACOWNIK.IdPracownika=PROWADZACY.IdPracownika
 
-
+/* Lab3_6 */
+SELECT ZALICZENIE.Ocena, STUDENT.Nazwisko as NazwiskoStudenta, PRZEDMIOT.Nazwa, PRACOWNIK.Nazwisko as NazwiskoProwadzacego
+FROM Uczelnia.dbo.Zaliczenie ZALICZENIE
+INNER JOIN Uczelnia.dbo.Zapisy ZAPISY ON ZALICZENIE.IdZapisu=ZAPISY.IdZapisu
+INNER JOIN Uczelnia.dbo.Student STUDENT ON STUDENT.NrIndeksu=ZAPISY.NrIndeksu
+INNER JOIN Uczelnia.dbo.ProwadzacyPrzedmiot PROWADZACY ON PROWADZACY.IdPrzydzialu=ZAPISY.IdPrzydzialu
+INNER JOIN Uczelnia.dbo.Pracownik PRACOWNIK ON PRACOWNIK.IdPracownika=PROWADZACY.IdPracownika
+INNER JOIN Uczelnia.dbo.Przedmiot PRZEDMIOT ON PRZEDMIOT.KodPrzedmiotu=PROWADZACY.KodPrzedmiotu
