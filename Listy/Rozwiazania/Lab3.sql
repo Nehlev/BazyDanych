2a04a05cf0b61be91321dd9f2161f9347e30e5a5
pkorycin <pawel.korycinski@nokia.com>
2019-04-23 22:03:55 +0200

    Lab3_Zad5

--- Listy/Rozwiazania/Lab3.sql
+++ Listy/Rozwiazania/Lab3.sql
@@ -2,6 +2,7 @@ SELECT * FROM Uczelnia.dbo.Pracownik
 SELECT * FROM Uczelnia.dbo.Student
 SELECT * FROM Uczelnia.dbo.Grupa
 SELECT * FROM Uczelnia.dbo.OddzialUczelni
+SELECT * FROM Uczelnia.dbo.Przedmiot
 
 /* Lab3_2 */
 SELECT Nazwisko, Nazwa
@@ -15,3 +16,11 @@ ON LHS.IdOddzialu=RHS.IdOddzialu WHERE RHS.Nazwisko IS NULL
 /* Lab3_4 */
 SELECT LHS.Nazwisko, RHS.IdGrupy FROM Uczelnia.dbo.Student LHS RIGHT JOIN Uczelnia.dbo.Grupa RHS
 ON LHS.IdGrupy=RHS.IdGrupy
+
+/* Lab3_5 */
+SELECT PRZEDMIOT.KodPrzedmiotu, PRZEDMIOT.Nazwa, CONCAT(PROWADZACY.RokPoczatek, '/', PROWADZACY.RokKoniec) AS RokAkademicki,/* Rok Akademicki */ PRZEDMIOT.Semestr, PRACOWNIK.Nazwisko
+FROM Uczelnia.dbo.Przedmiot PRZEDMIOT
+INNER JOIN Uczelnia.dbo.ProwadzacyPrzedmiot PROWADZACY ON PROWADZACY.KodPrzedmiotu=PRZEDMIOT.KodPrzedmiotu
+INNER JOIN Uczelnia.dbo.Pracownik PRACOWNIK ON PRACOWNIK.IdPracownika=PROWADZACY.IdPracownika
+
+
