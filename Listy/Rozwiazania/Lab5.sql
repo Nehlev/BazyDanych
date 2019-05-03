17b1309b349bac81f1ab45b485b6f650be67e131
pkorycin <pawel.korycinski@nokia.com>
2019-05-03 16:02:03 +0200

    Lab5_Zad3

--- Listy/Rozwiazania/Lab5.sql
+++ Listy/Rozwiazania/Lab5.sql
@@ -2,17 +2,10 @@
 DROP TABLE [TypSali]
 DROP PROC TypSali_wstawianie
 
-CREATE TABLE [TypSali] (
-	[KodTyp] [char] (3) COLLATE Polish_CI_AS NOT NULL ,
-	[Typ] [nvarchar] (50) COLLATE Polish_CI_AS NOT NULL ,
+CREATE TABLE [dbo].[TypSali] (
+	[KodTyp] [char] (3) NOT NULL PRIMARY KEY,
+	[Typ] [nvarchar] (50) COLLATE Polish_CI_AS NOT NULL
 ) ON [PRIMARY]
-GO
-
-ALTER TABLE [TypSali] WITH NOCHECK ADD
-	CONSTRAINT [PK_TypSali] PRIMARY KEY CLUSTERED
-	(
-		[KodTyp]
-	) ON [PRIMARY]
 GO
 
 CREATE PROC TypSali_wstawianie
@@ -26,19 +19,28 @@ EXEC TypSali_wstawianie 'LAB', 'Laboratoryjna'
 EXEC TypSali_wstawianie 'WYK', 'Wyk³adowa'
 GO
 
+SELECT * FROM [dbo].[TypSali]
+
 /* Lab 5_2 */
-DROP TABLE [Sala]
-CREATE TABLE [Sala] (
-	[IdSali] [int] NOT NULL,
+DROP TABLE [dbo].[Sala]
+CREATE TABLE [dbo].[Sala] (
+	[IdSali] [int] NOT NULL PRIMARY KEY,
 	[IdOddzialu] [tinyint] NOT NULL,
 	[Pojemnosc] [int] NOT NULL,
 	[KodTyp] [char] (3) NOT NULL
 ) ON [PRIMARY]
 GO
 
-ALTER TABLE [Sala] WITH NOCHECK ADD
-	CONSTRAINT [PK_Sala] PRIMARY KEY CLUSTERED
+ALTER TABLE [dbo].[Sala] ADD
+	CONSTRAINT [FK_Sala_IdOddzialu] FOREIGN KEY
 	(
-		[IdSali]
-	) ON [PRIMARY]
+		[IdOddzialu]
+	) REFERENCES OddzialUczelni(IdOddzialu)
+GO
+
+ALTER TABLE [dbo].[Sala] ADD
+	CONSTRAINT [FK_Sala_KodTyp] FOREIGN KEY
+	(
+		[KodTyp]
+	) REFERENCES TypSali(KodTyp)
 GO
