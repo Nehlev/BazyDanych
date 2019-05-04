3c85eea437de6602c1a561e7e18abf2c0969ea56
pkorycin <pawel.korycinski@nokia.com>
2019-05-04 21:06:18 +0200

    Lab7_8_Zad3

--- Listy/Rozwiazania/Lab7_8.sql
+++ Listy/Rozwiazania/Lab7_8.sql
@@ -3,7 +3,7 @@
 DROP VIEW V_Pracownicy_I_Oddzial
 GO 
 */
-CREATE VIEW V_Pracownicy_I_Oddzial AS SELECT
+CREATE VIEW V_Pracownicy_I_Oddzial as SELECT
 
 PRACOWNIK.Tytul, PRACOWNIK.Nazwisko, PRACOWNIK.Imie, PRACOWNIK.Stanowisko, PRACOWNIK.TelefonKom, PRACOWNIK.Email,
 ODDZIAL.Nazwa as MiejscePracy_Nazwa, ODDZIAL.Miasto as MiejscePracy_Adres, ODDZIAL.Telefon as MiejscePracy_Telefon, ODDZIAL.Email as MiejscePracy_Email
@@ -19,7 +19,7 @@ SELECT * FROM V_Pracownicy_I_Oddzial
 DROP VIEW V_Przedmiot_I_Prowadzacy
 GO
 */
-CREATE VIEW V_Przedmiot_I_Prowadzacy AS SELECT
+CREATE VIEW V_Przedmiot_I_Prowadzacy as SELECT
 
 PRZEDMIOT.Nazwa, PRZEDMIOT.KodPrzedmiotu, PRZEDMIOT.Semestr, PRZEDMIOT.ECTS, PRZEDMIOT.FormaZajec,
 PRACOWNIK.Imie as Pracownik_Imie, PRACOWNIK.Nazwisko as Pracownik_Nazwisko
@@ -32,3 +32,24 @@ GO
 SELECT * FROM V_Przedmiot_I_Prowadzacy
 
 /**************** Lab 7_8_3 ****************/
+/*
+DROP VIEW V_Zaliczenie_I_Student_I_Prowadzacy
+GO
+*/
+CREATE VIEW V_Zaliczenie_I_Student_I_Prowadzacy as SELECT
+
+PROWADZACY.KodPrzedmiotu, PRZEDMIOT.Nazwa,
+ZALICZENIE.Ocena, ZALICZENIE.TypZaliczenia, ZALICZENIE.Termin,
+STUDENT.Imie as StudentImie, STUDENT.Nazwisko as StudentNazwisko,
+PRACOWNIK.Imie as ProwadzacyImie, PRACOWNIK.Nazwisko as ProwadzacyNazwisko
+
+FROM Zaliczenie as ZALICZENIE
+LEFT JOIN Zapisy as ZAPISY ON ZALICZENIE.IdZapisu=ZAPISY.IdZapisu
+JOIN Student as STUDENT ON ZAPISY.NrIndeksu=STUDENT.NrIndeksu
+JOIN ProwadzacyPrzedmiot as PROWADZACY ON ZAPISY.IdPrzydzialu=PROWADZACY.IdPrzydzialu
+JOIN Pracownik as PRACOWNIK ON PROWADZACY.IdPracownika=PRACOWNIK.IdPracownika
+JOIN Przedmiot as PRZEDMIOT ON PRZEDMIOT.KodPrzedmiotu=PROWADZACY.KodPrzedmiotu
+GO
+
+
+/**************** Lab 7_8_4 ****************/
